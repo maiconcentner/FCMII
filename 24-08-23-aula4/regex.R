3f4b67a0d492c97fd89ad1f8034457ee55d48c2b
maiconcentner <maiconcentner@gmail.com>
2023-08-24 18:52:41 -0300

    tarefa 24-08-23

--- 24-08-23-aula4/regex.R
+++ 24-08-23-aula4/regex.R
@@ -1,3 +1,6 @@
+
+##################################
+# Carregamento
 library(tidyverse)
 library(pdftools)
 
@@ -6,6 +9,35 @@ setwd("D:\\Mestrado\\2Sem_23\\FCMII\\24-08-23-aula4")
 pdf <- pdf_text("cadastro.pdf")
 pdf <- str_split_1(pdf,"\\n")
 pdf
+
+###################################
+
+## 24/08/2023 TAREFA ABAIXO
+# extraindo o endereço
+endereco <- pdf[grepl("Endereço:",pdf)]
+endereco <- gsub("Endereço:","",endereco)
+endereco <- gsub("CEP.*[0-9]$","",endereco)
+endereco <- gsub("\\.$","",endereco)
+endereco <- trimws(endereco,"both")
+
+endereco1 <- gsub(",.*","", endereco) # nome da rua
+
+num_endereco <- gsub("[^0-9]","",endereco)
+num_endereco <- gsub("^$","S/N", num_endereco) # numero
+
+bairro <- gsub(".*,(.*)","\\1",endereco) # bairro
+bairro <- trimws(bairro,"both")
+
+###################################
+
+### TEREFA DO DIA 17/08/2023
+# Extraindo o CEP
+cep <- pdf[grepl("CEP:", pdf)]
+cep <- gsub("CEP:", "", cep)
+cep <- gsub("[^0-9-]", "", cep)
+cep <- trimws(cep, "both")
+
+###################################
 
 # extraindo o nome
 nome <-pdf[grepl("[nN]ome:", pdf)]
@@ -19,25 +51,6 @@ nome <- trimws(nome,"both") # tira espacos sobrnado dos lados
 apelido <-pdf[grepl("[nN]ome:", pdf)]
 apelido <- str_extract(apelido, "\\(.*\\)")
 apelido <- gsub("\\(\\w+ |\\)","",apelido)
-
-### TEREFA DO DIA 17/08/2023
-# Extraindo o CEP
-cep <- pdf[grepl("CEP:", pdf)]
-cep <- gsub("CEP:", "", cep)
-cep <- gsub("[^0-9-]", "", cep)  # Removendo tudo exceto números e o caractere de traço
-
-# Limpando espaços em branco
-cep <- trimws(cep, "both")
-cep
-
-
-## 24/08/2023
-# extraindo o endereço
-endereco <- pdf[grepl("Endereço:",pdf)]
-endereco <- gsub("Endereço:","",endereco)
-endereco <- gsub("CEP.*[0-9]$","",endereco)
-endereco <- gsub("\\.$","",endereco)
-endereco <- trimws(endereco,"both")
 
 #extraindo telefone
 telefone <- pdf[grepl("Tel:|Telefone:",pdf)]
@@ -55,11 +68,12 @@ cpf <- trimws(cpf,"both")
 # extraindo data de nascimento
 data_nascimento <- pdf[grepl("Data de nascimento:|Dt nasc:", pdf)]
 data_nascimento <- gsub("Data de nascimento:|Dt nasc:","", data_nascimento)
-
+obs_data <- gsub("[0-9-]","",data_nascimento)
 
 # Criando a base de dados
-cadastro <- cbind(nome,apelido,data_nascimento, endereco, cep,telefone,cpf)
+cadastro <- cbind(nome,apelido,data_nascimento, endereco1,num_endereco,bairro, cep,telefone,cpf)
 cadastro <- as.data.frame(cadastro)
+
 
 
 
