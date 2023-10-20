2b1fb37395af610e10374e7a5d0ba3146099c668
maiconcentner <maiconcentner@gmail.com>
2023-10-20 17:43:37 -0300

    tarefa SQL 20-1023

--- 20-10-23-aula7/tarefa-dia-20-10-23.sql
+++ 20-10-23-aula7/tarefa-dia-20-10-23.sql
@@ -1,3 +1,14 @@
+-- TAREFA DO DIA 20/10/23
+-- Verificar o valor máximo e mínimo de cada marca de veículo vendido
+SELECT 
+	brand,
+	MAX(price) AS maior_preco,
+	MIN(price) AS menor_preco,
+	COUNT(brand) AS total_vendas
+FROM sales.products
+GROUP BY brand
+ORDER BY total_vendas DESC;
+
 -- Tabelas da sales
 /* SELECT *
 FROM sales.funnel */
@@ -33,5 +44,4 @@ GROUP BY sales.customers.state*/
 --GROUP BY sales.customers.state
 --HAVING YEAR(sales.funnel.visit_page_date) = 2020
 
--- TAREFA DO DIA 20/10/23
--- Verificar o valor máximo e mínimo de cada produto vendido
+
